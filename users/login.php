ef2148c90d8198457aa5740b4f823776c367d2ef
José Pedro Machado <pedrorodriguesmachado@gmail.com>
2020-12-21 20:16:20 +0000

    Add files via upload

--- users/login.php
+++ users/login.php
@@ -29,7 +29,7 @@ includeHook($hooks,'pre');
 if(ipCheckBan()){Redirect::to($us_url_root.'usersc/scripts/banned.php');die();}
 $errors = $successes = [];
 if (Input::get('err') != '') {
-    $errors[] = Input::get('err');
+  $errors[] = Input::get('err');
 }
 $reCaptchaValid=FALSE;
 if($user->isLoggedIn()) Redirect::to($us_url_root.'index.php');
@@ -42,40 +42,40 @@ if (!empty($_POST['login_hook'])) {
 
   //Check to see if recaptcha is enabled
   if($settings->recaptcha == 1){
-  if(!function_exists('post_captcha')){
-    function post_captcha($user_response) {
-    global $settings;
-    $fields_string = '';
-    $fields = array(
-        'secret' => $settings->recap_private,
-        'response' => $user_response
-    );
-    foreach($fields as $key=>$value)
-    $fields_string .= $key . '=' . $value . '&';
-    $fields_string = rtrim($fields_string, '&');
+    if(!function_exists('post_captcha')){
+      function post_captcha($user_response) {
+        global $settings;
+        $fields_string = '';
+        $fields = array(
+          'secret' => $settings->recap_private,
+          'response' => $user_response
+        );
+        foreach($fields as $key=>$value)
+        $fields_string .= $key . '=' . $value . '&';
+        $fields_string = rtrim($fields_string, '&');
 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
-    curl_setopt($ch, CURLOPT_POST, count($fields));
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
+        curl_setopt($ch, CURLOPT_POST, count($fields));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
 
-    $result = curl_exec($ch);
-    curl_close($ch);
-    return json_decode($result, true);
-}
-}
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($result, true);
+      }
+    }
 
-// Call the function post_captcha
-$res = post_captcha($_POST['g-recaptcha-response']);
+    // Call the function post_captcha
+    $res = post_captcha($_POST['g-recaptcha-response']);
 
-if (!$res['success']) {
-    // What happens when the reCAPTCHA is not properly set up
-    echo 'reCAPTCHA error: Check to make sure your keys match the registered domain and are in the correct locations. You may also want to doublecheck your code for typos or syntax errors.';
-}else{
- $reCaptchaValid=TRUE;
-}
-}
+    if (!$res['success']) {
+      // What happens when the reCAPTCHA is not properly set up
+      echo 'reCAPTCHA error: Check to make sure your keys match the registered domain and are in the correct locations. You may also want to doublecheck your code for typos or syntax errors.';
+    }else{
+      $reCaptchaValid=TRUE;
+    }
+  }
   if($reCaptchaValid || $settings->recaptcha == 0 || $settings->recaptcha == 2 ){ //if recaptcha valid or recaptcha disabled
 
     $validate = new Validate();
@@ -93,109 +93,121 @@ if (!$res['success']) {
           $hooks =  getMyHooks(['page'=>'loginSuccess']);
           includeHook($hooks,'body');
           $dest = sanitizedDest('dest');
-              # if user was attempting to get to a page before login, go there
-              $_SESSION['last_confirm']=date("Y-m-d H:i:s");
+          # if user was attempting to get to a page before login, go there
+          $_SESSION['last_confirm']=date("Y-m-d H:i:s");
 
-              if (!empty($dest)) {
-                $redirect=html_entity_decode(Input::get('redirect'));
-                if(!empty($redirect) || $redirect!=='') Redirect::to($redirect);
-                else Redirect::to($dest);
-              } elseif (file_exists($abs_us_root.$us_url_root.'usersc/scripts/custom_login_script.php')) {
+          if (!empty($dest)) {
+            $redirect=html_entity_decode(Input::get('redirect'));
+            if(!empty($redirect) || $redirect!=='') Redirect::to($redirect);
+            else Redirect::to($dest);
+          } elseif (file_exists($abs_us_root.$us_url_root.'usersc/scripts/custom_login_script.php')) {
 
-                # if site has custom login script, use it
-                # Note that the custom_login_script.php normally contains a Redirect::to() call
-                require_once $abs_us_root.$us_url_root.'usersc/scripts/custom_login_script.php';
-              } else {
-                if (($dest = Config::get('homepage')) ||
-                ($dest = 'account.php')) {
-                  #echo "DEBUG: dest=$dest<br />\n";
-                  #die;
-                  Redirect::to($dest);
-                }
-              }
-
+            # if site has custom login script, use it
+            # Note that the custom_login_script.php normally contains a Redirect::to() call
+            require_once $abs_us_root.$us_url_root.'usersc/scripts/custom_login_script.php';
           } else {
-            $eventhooks =  getMyHooks(['page'=>'loginFail']);
-            includeHook($eventhooks,'body');
-            logger("0","Login Fail","A failed login on login.php");
-            $msg = lang("SIGNIN_FAIL");
-            $msg2 = lang("SIGNIN_PLEASE_CHK");
-            $errors[] = '<strong>'.$msg.'</strong>'.$msg2;
+            if (($dest = Config::get('homepage')) ||
+            ($dest = 'account.php')) {
+              #echo "DEBUG: dest=$dest<br />\n";
+              #die;
+              Redirect::to($dest);
+            }
           }
+
+        } else {
+          $eventhooks =  getMyHooks(['page'=>'loginFail']);
+          includeHook($eventhooks,'body');
+          logger("0","Login Fail","A failed login on login.php");
+          $msg = lang("SIGNIN_FAIL");
+          $msg2 = lang("SIGNIN_PLEASE_CHK");
+          $errors[] = '<strong>'.$msg.'</strong>'.$msg2;
         }
       }
     }
-    if (empty($dest = sanitizedDest('dest'))) {
-      $dest = '';
-    }
-    $token = Token::generate();
-    ?>
-    <div id="page-wrapper">
-      <div class="container">
-        <?=resultBlock($errors,$successes);?>
-        <div class="row">
-          <div class="col-sm-12">
-            <?php
-            includeHook($hooks,'body');
-            ?>
-          </div>
+  }
+  if (empty($dest = sanitizedDest('dest'))) {
+    $dest = '';
+  }
+  $token = Token::generate();
+  ?>
+  <!-- Login page design -->
+  <div class="container">
+    <?=resultBlock($errors,$successes);?>
+    <?php includeHook($hooks,'body'); ?>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col-md-6 col-lg-4">
+      <div class="card">
+        <div class="card-header">
+          <h2 class="form-signin-heading" style="text-align:center"> <?=lang("SIGNIN_TITLE","");?></h2>
         </div>
-        <div class="row">
-          <div class="col-sm-12">
-            <form name="login" id="login-form" class="form-signin" action="login.php" method="post">
-              <h2 class="form-signin-heading"></i> <?=lang("SIGNIN_TITLE","");?></h2>
-              <input type="hidden" name="dest" value="<?= $dest ?>" />
-
-              <div class="form-group">
-                <label for="username" id="username-label" ><?=lang("SIGNIN_UORE")?></label>
-                <input  class="form-control" type="text" name="username" id="username" placeholder="<?=lang("SIGNIN_UORE")?>" required autofocus autocomplete="username">
+        <div class="card-body">
+          <form name="login" id="login-form" class="form-signin" action="login.php" method="post">
+            <input type="hidden" name="dest" value="<?= $dest ?>" />
+            <div class="form-group">
+              <i class="fa fas fa-user prefix"></i>
+              <label for="username" id="username-label" ><?=lang("SIGNIN_UORE")?></label>
+              <input  class="form-control" type="text" name="username" id="username" placeholder="<?=lang("SIGNIN_UORE")?>" required autofocus autocomplete="username">
+            </div>
+            <div class="form-group">
+              <i class="fa fas fa-lock prefix"></i>
+              <label for="password" id="password-label"><?=lang("SIGNIN_PASS")?></label>
+              <input type="password" class="form-control"  name="password" id="password"  placeholder="<?=lang("SIGNIN_PASS")?>" required autocomplete="current-password">
+              <div class="row mt-2">
+                <div class="col">
+                  <a class="" href='../users/forgot_password.php'><i class="fa fa-wrench"></i> <?=lang("SIGNIN_FORGOTPASS","");?></a>
+                </div>
               </div>
-
-              <div class="form-group">
-                <label for="password" id="password-label"><?=lang("SIGNIN_PASS")?></label>
-                <input type="password" class="form-control"  name="password" id="password"  placeholder="<?=lang("SIGNIN_PASS")?>" required autocomplete="current-password">
-              </div>
-              <?php   includeHook($hooks,'form');?>
-                <input type="hidden" name="login_hook" value="1">
-                <input type="hidden" name="csrf" value="<?=$token?>">
-                <input type="hidden" name="redirect" value="<?=Input::get('redirect')?>" />
+            </div>
+            <?php   includeHook($hooks,'form');?>
+            <input type="hidden" name="login_hook" value="1">
+            <input type="hidden" name="csrf" value="<?=$token?>">
+            <input type="hidden" name="redirect" value="<?=Input::get('redirect')?>" />
+            <div class="row">
+              <div class="col-6">
                 <button class="submit  btn  btn-primary" id="next_button" type="submit"><i class="fa fa-sign-in"></i> <?=lang("SIGNIN_BUTTONTEXT","");?></button>
-                <?php
-                if($settings->recaptcha == 1){
-                  ?>
-                  <div class="g-recaptcha" data-sitekey="<?=$settings->recap_public; ?>" data-bind="next_button" data-callback="submitForm"></div>
+              </div>
+              <div class="col-6">
+                <?php if($settings->registration==1) {?>
+                  <a class="btn" href='../users/join.php'><i class="fa fa-plus-square"></i> <?=lang("SIGNUP_TEXT","");?></a>
                 <?php } ?>
-              </form>
+              </div>
             </div>
           </div>
-          <div class="row">
-            <div class="col-sm-6"><br>
-              <a class="pull-left" href='../users/forgot_password.php'><i class="fa fa-wrench"></i> <?=lang("SIGNIN_FORGOTPASS","");?></a>
-              <br><br>
-            </div>
-            <?php if($settings->registration==1) {?>
-              <div class="col-sm-6"><br>
-                <a class="pull-right" href='../users/join.php'><i class="fa fa-plus-square"></i> <?=lang("SIGNUP_TEXT","");?></a><br><br>
-              </div><?php } ?>
-              <?php   includeHook($hooks,'bottom');?>
-                <?php languageSwitcher();?>
-            </div>
-          </div>
-        </div>
+          <?php
+          if($settings->recaptcha == 1){
+            ?>
+            <div class="g-recaptcha" data-sitekey="<?=$settings->recap_public; ?>" data-bind="next_button" data-callback="submitForm"></div>
+          <?php } ?>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row mt-3">
+  <div class="col">
+    <?php   includeHook($hooks,'bottom');?>
+  </div>
+</div>
+<div class="row mt-4">
+  <div class="col">
+    <?php languageSwitcher();?>
+  </div>
+</div>
+</div>
+<?php require_once $abs_us_root.$us_url_root.'usersc/templates/'.$settings->template.'/container_close.php'; //custom template container ?>
 
-        <?php require_once $abs_us_root.$us_url_root.'usersc/templates/'.$settings->template.'/container_close.php'; //custom template container ?>
+<!-- footers -->
+<?php require_once $abs_us_root.$us_url_root.'users/includes/page_footer.php'; // the final html footer copyright row + the external js calls ?>
 
-        <!-- footers -->
-        <?php require_once $abs_us_root.$us_url_root.'users/includes/page_footer.php'; // the final html footer copyright row + the external js calls ?>
+<!-- Place any per-page javascript here -->
 
-        <!-- Place any per-page javascript here -->
-
-        <?php   if($settings->recaptcha == 1){ ?>
-          <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-          <script>
-          function submitForm() {
-            document.getElementById("login-form").submit();
-          }
-          </script>
-        <?php } ?>
-        <?php require_once $abs_us_root.$us_url_root.'usersc/templates/'.$settings->template.'/footer.php'; //custom template footer?>
+<?php   if($settings->recaptcha == 1){ ?>
+  <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+  <script>
+  function submitForm() {
+    document.getElementById("login-form").submit();
+  }
+  </script>
+<?php } ?>
+<?php require_once $abs_us_root.$us_url_root.'usersc/templates/'.$settings->template.'/footer.php'; //custom template footer?>
