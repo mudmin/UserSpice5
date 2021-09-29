86050ebea8f2627b3c5700e6044efe706043dddf
Robert McIntosh <rmcintosh@remabledesigns.com>
2021-09-29 00:13:37 -0400

    Allows for Uninstall Options
    - Added dialog to ask user to purge files if plugin is installed
    - Purges files if plugin is uninstalled
    - Allows plugins to have a dialog.uninstall.html file that will be displayed when the user uninstalled the app. This can allow the plugin developer to ask the user for specific things like cleaning up the database.

--- users/views/_admin_plugins.php
+++ users/views/_admin_plugins.php
@@ -35,17 +35,88 @@ foreach ($dirs as $d) {
 $pluginsC = sizeof($plugins);
 
 if (!empty($_POST)) {
-  $disable = Input::get('disable');
-  $activate = Input::get('activate');
-  $uninstall = Input::get('uninstall');
-  $install = Input::get('install');
+  $token = $_POST['csrf'];
+  if(!Token::check($token)){
+    include($abs_us_root.$us_url_root.'usersc/scripts/token_error.php');
+  }
+
+
   $plugin = Input::get('plugin');
+
+  if(!empty($_POST['lock'])){
+    $action = Input::get('action');
+    $file = $abs_us_root.$us_url_root."usersc/plugins/".$plugin."/.noupdate";
+    if($action == "unlockme"){
+      unlink($file);
+      Redirect::to('admin.php?view=plugins&err=' . $plugin . ' unlocked');
+    }
+
+    if($action == "lockme"){
+      $write = fopen($file,"w");
+      fwrite($write,"");
+      fclose($write);
+      Redirect::to('admin.php?view=plugins&err=' . $plugin . ' has been locked');
+    }
+  }
+  $activate = Input::get('activate');
+  $delete = Input::get('delete');
+  $deactivate = Input::get('deactivate');
+  $install = Input::get('install');
+  $purge = Input::get('purge');
+
   $jump = Input::get('jump');
+
+
+  if ($delete != '' || $purge != '') {
+    if (file_exists($abs_us_root . $us_url_root . 'usersc/plugins/' . $plugin . '/uninstall.php')) {
+      include $abs_us_root . $us_url_root . 'usersc/plugins/' . $plugin . '/uninstall.php';
+    }
+    $usplugins[$plugin] = 2;
+    $db->update('us_plugins', ['plugin', '=', $plugin], ['status' => 'uninstalled']);
+    write_php_ini($usplugins, $abs_us_root . $us_url_root . 'usersc/plugins/plugins.ini.php');
+    if (file_exists($abs_us_root . $us_url_root . 'usersc/plugins/' . $plugin . '/uninstall.php')) {
+      include $abs_us_root . $us_url_root . 'usersc/plugins/' . $plugin . '/uninstall.php';
+    }
+    // TODO: Is this still Needed? Code can be added to uninstall.php?
+    // ? Only run if user selected to purge files
+    if (Input::get('purgePlugin') || $purge != '') {
+      if (file_exists($abs_us_root . $us_url_root . 'usersc/plugins/' . $plugin . '/delete.php')) {
+        include $abs_us_root . $us_url_root . 'usersc/plugins/' . $plugin . '/delete.php';
+      }
+      // Verify that user has chosen to purge the plugin
+      if(is_dir($abs_us_root . $us_url_root . 'usersc/plugins/' . $plugin)) {
+        $files = new RecursiveIteratorIterator(
+          new RecursiveDirectoryIterator($abs_us_root . $us_url_root . 'usersc/plugins/' . $plugin, RecursiveDirectoryIterator::SKIP_DOTS),
+          RecursiveIteratorIterator::CHILD_FIRST
+        );
+
+        foreach ($files as $fileinfo) {
+          $todo = ($fileinfo->isDir() ? 'rmdir' : 'unlink');
+          $todo($fileinfo->getRealPath());
+        }
+
+        rmdir($abs_us_root . $us_url_root . 'usersc/plugins/' . $plugin);
+      }
+    }
+    Redirect::to('admin.php?view=plugins&err=' . $plugin . ' deleted');
+  }
+
+  if ($deactivate != '') {
+    $usplugins[$plugin] = 0;
+    $db->update('us_plugins', ['plugin', '=', $plugin], ['status' => 'uninstalled']);
+    write_php_ini($usplugins, $abs_us_root . $us_url_root . 'usersc/plugins/plugins.ini.php');
+    if (file_exists($abs_us_root . $us_url_root . 'usersc/plugins/' . $plugin . '/uninstall.php')) {
+      include $abs_us_root . $us_url_root . 'usersc/plugins/' . $plugin . '/uninstall.php';
+    }
+    Redirect::to('admin.php?view=plugins&err=' . $plugin . ' deactivated. You may click the trash can icon to delete the plugin' . $jump);
+  }
 
 
   if ($activate != '') {
     $usplugins[$plugin] = 1;
+    $db->update('us_plugins', ['plugin', '=', $plugin], ['status' => 'active']);
     write_php_ini($usplugins, $abs_us_root . $us_url_root . 'usersc/plugins/plugins.ini.php');
+
     if (file_exists($abs_us_root . $us_url_root . 'usersc/plugins/' . $plugin . '/activate.php')) {
       include $abs_us_root . $us_url_root . 'usersc/plugins/' . $plugin . '/activate.php';
     }
@@ -54,25 +125,9 @@ if (!empty($_POST)) {
     }
     $pluginId = $db->query("SELECT id FROM us_plugins WHERE plugin = ?",[$plugin])->first();
     $db->update('us_plugins',$pluginId->id,['last_check'=>date("Y-m-d H:i:s")]);
-    Redirect::to('admin.php?view=plugins&err=' . $plugin . ' activated' . $jump);
-  }
 
-  if ($disable != '') {
-    $usplugins[$plugin] = 0;
-    write_php_ini($usplugins, $abs_us_root . $us_url_root . 'usersc/plugins/plugins.ini.php');
-    $db->update('us_plugins', ['plugin', '=', $plugin], ['status' => 'disabled']);
-    Redirect::to('admin.php?view=plugins&err=' . $plugin . ' disabled' . $jump);
-  }
 
-  if ($uninstall != '') {
-    $usplugins[$plugin] = 2;
-    $db->update('us_plugins', ['plugin', '=', $plugin], ['status' => 'uninstalled']);
-    write_php_ini($usplugins, $abs_us_root . $us_url_root . 'usersc/plugins/plugins.ini.php');
-    if (file_exists($abs_us_root . $us_url_root . 'usersc/plugins/' . $plugin . '/uninstall.php')) {
-      echo "file exists";
-      include $abs_us_root . $us_url_root . 'usersc/plugins/' . $plugin . '/uninstall.php';
-    }
-    Redirect::to('admin.php?view=plugins&err=' . $plugin . ' uninstalled. You may delete the plugin files if you wish.' . $jump);
+    Redirect::to('admin.php?view=plugins&err=' . $plugin . ' Activated.' . $jump);
   }
 
   if ($install != '') {
@@ -130,6 +185,15 @@ $token = Token::generate();
               <tbody>
                 <?php
                   foreach ($plugins as $t) {
+                    if(!file_exists($abs_us_root . $us_url_root . 'usersc/plugins/' . $t . '/info.xml')) { ?>
+                      <tr>
+                        <td colspan="4">
+                          Loading <?=$t?> has failed, XML file does not exist
+                        </td>
+                      </tr>
+                    <?php
+                      continue;
+                    }
                     $xml = simplexml_load_file($abs_us_root . $us_url_root . 'usersc/plugins/' . $t . '/info.xml');
                     if (file_exists($abs_us_root . $us_url_root . 'usersc/plugins/' . $t . '/logo.png')) {
                       $img_src = $us_url_root . 'usersc/plugins/' . $t . '/logo.png';
@@ -168,33 +232,65 @@ $token = Token::generate();
                     </td>
                     <td class="text-center">
                       <h4><?php pluginStatus($usplugins[$t]); ?></h4>
+                      <form class="" action="" method="post">
+                        <input type="hidden" name="csrf" value="<?=Token::generate();?>">
+                        <input type="hidden" name="lock" value="lock">
+                        <input type="hidden" name="plugin" value="<?= $t ?>">
+                        <?php if(file_exists($abs_us_root.$us_url_root."usersc/plugins/".$t."/.noupdate")){ ?>
+                        <button type="submit" name="action" value="unlockme" class="btn" title="Unlock this plugin to allow Spice Shaker to update it.">
+                          <i class="fa fa-lock" aria-hidden="true"></i>
+                        </button>
+                      <?php } else { ?>
+                        <button type="submit" name="action" value="lockme" class="btn" title="Lock this plugin to prevent plugin from being updated">
+                          <i class="fa fa-unlock" aria-hidden="true"></i>
+                        </button>
+                      <?php } ?>
+                      </form>
                     </td>
                     <td>
                       <div class="d-flex justify-content-center">
                         <form class="" action="" method="post">
+                          <input type="hidden" name="csrf" value="<?=Token::generate();?>">
                           <input type="hidden" name="jump" value="#ctrl-<?= $xml->name ?>">
                           <input type="hidden" name="plugin" value="<?= $t ?>">
-                          <?php if ($usplugins[$t] == 1) { ?>
-                            <button type="submit" name="disable" value="Disable" class="btn btn-outline-dark" title="Disable">
-                              <i class="fa fa-ban" aria-hidden="true"></i>
-                            </button>
-                            <a class="btn btn-outline-primary" title="Configure" href="<?= $us_url_root . 'users/admin.php?view=plugins_config&plugin=' . $t ?>" role="button">
-                              <i class="fa fa-cogs" aria-hidden="true"></i>
-                            </a>
-                          <?php } ?>
-                          <?php if ($usplugins[$t] == 0) { ?>
-                            <button type="submit" name="activate" value="Activate" class="btn btn-outline-success" title="Activate">
-                              <i class="fa fa-toggle-on" aria-hidden="true"></i>
-                            </button>
-                            <button type="submit" name="uninstall" value="Uninstall" class="btn btn-outline-danger" title="Uninstall">
-                              <i class="fa fa-trash-o" aria-hidden="true"></i>
-                            </button>
-                          <?php } ?>
-                          <?php if ($usplugins[$t] != 0 && $usplugins[$t] != 1) { ?>
-                            <button type="submit" name="install" value="Install" class="btn btn-outline-primary" title="Install">
-                              <i class="fa fa-download" aria-hidden="true"></i>
-                            </button>
-                            <!-- <input type="submit" name="uninstall" value="Uninstall" class="btn btn-default"> -->
+                          <input type="hidden" name="hasUninstallOptions" value="<?php echo file_exists($abs_us_root . $us_url_root . 'usersc/plugins/' . $t . '/dialog.uninstall.html') ? 'true' : 'false'; ?>" />
+                          <?php if(!file_exists($abs_us_root.$us_url_root."usersc/plugins/".$t."/.noupdate")) { ?>
+                            <?php if ($usplugins[$t] == 1) {  // plugin installed and active ?>
+                              <button type="submit" name="deactivate" value="Deactivate" class="btn btn-outline-dark" title="Deactivate">
+                                <i class="fa fa-ban" aria-hidden="true"></i>
+                              </button>
+                              <a class="btn btn-outline-primary" title="Configure" href="<?= $us_url_root . 'users/admin.php?view=plugins_config&plugin=' . $t ?>" role="button">
+                                <i class="fa fa-cogs" aria-hidden="true"></i>
+                              </a>
+                            <?php }else if ($usplugins[$t] == 0) { //plugin installed and inactive ?>
+                              <button type="submit" name="activate" value="Activate" class="btn btn-outline-success" title="Activate">
+                                <i class="fa fa-toggle-on" aria-hidden="true"></i>
+                              </button>
+                              <button type="button" class="btn btn-outline-danger" data-toggle="modal" data-target="#uninstallPlugin">
+                                <i class="fa fa-trash-o" aria-hidden="true"></i>
+                              </button>
+                            <?php } ?>
+                            <?php if ($usplugins[$t] != 0 && $usplugins[$t] != 1) { // plugin not installed ?>
+                              <button type="submit" name="install" value="Install" class="btn btn-outline-primary" title="Install">
+                                <i class="fa fa-download" aria-hidden="true"></i>
+                              </button>
+                              <button type="submit" name="purge" value="Purge" class="btn btn-outline-danger" title="Purge"
+                                onclick="return confirm('If you continue, the plugin files will be deleted.  The plugin may also choose to delete/clean up the data it created in your database.  To see which actions would be performed, take a look at the delete.php file in the plugin folder (if it exists).  This cannot be undone.');">
+                                <i class="fa fa-trash-o" aria-hidden="true"></i>
+                              </button>
+                              <!-- <input type="submit" name="uninstall" value="Uninstall" class="btn btn-default"> -->
+                            <?php } ?>
+                          <?php }else{ ?>
+                            <?php if ($usplugins[$t] == 1) {  // plugin installed and active ?>
+                              <a class="btn btn-outline-primary" title="Configure" href="<?= $us_url_root . 'users/admin.php?view=plugins_config&plugin=' . $t ?>" role="button">
+                                <i class="fa fa-cogs" aria-hidden="true"></i>
+                              </a>
+                            <?php }else if ($usplugins[$t] == 0) { //plugin installed and inactive ?>
+                              <span class="badge badge-info">Installed and Inactive</span>
+                            <?php } ?>
+                            <?php if ($usplugins[$t] != 0 && $usplugins[$t] != 1) { // plugin not installed ?>
+                              <span class="badge badge-warning">Not Installed</span>
+                            <?php } ?>
                           <?php } ?>
                         </form>
                       </div>
@@ -203,6 +299,57 @@ $token = Token::generate();
                 <?php } ?>
               </tbody>
             </table>
+            <div class="modal fade" id="uninstallPlugin" tabindex="-1" aria-labelledby="uninstallPluginLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                <form class="modal-content" action="" method="post">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="uninstallPluginLabel">Uninstall: </h5>
+                    <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                    <!-- plugin details -->
+                    <input type="hidden" name="csrf">
+                    <input type="hidden" name="jump">
+                    <input type="hidden" name="plugin">
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" value="true" id="purgePlugin" name="purgePlugin">
+                      <label class="form-check-label" for="purgePlugin">
+                        Purge Plugin Files?
+                      </label>
+                    </div>
+                    <div class="plugin-options"></div>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" name="delete" value="Delete" class="btn btn-danger" title="Delete">Uninstall Plugin</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <script type="text/javascript">
+              $('#uninstallPlugin').on('show.bs.modal', function (event) {
+                // do something...
+                var $pluginForm = $(event.relatedTarget).closest('form');
+                var $modelForm = $(event.currentTarget).find('form');
+
+                $modelForm.closest('.modal').find('#uninstallPluginLabel').text('Uninstall: ' + $pluginForm.closest('tr').find('h4').first().text());
+
+                // Set Plugin Values
+                $modelForm.find('[name="csrf"]').val($pluginForm.find('[name="csrf"]').val());
+                $modelForm.find('[name="jump"]').val($pluginForm.find('jump').val());
+                $modelForm.find('[name="plugin"]').val($pluginForm.find('[name="plugin"]').val());
+
+                // Clear Plugin options
+                $modelForm.find('.modal-body .plugin-options').html("");
+
+                // Check if Plugin has Uninstall Options, If so, Append them.
+                if ($pluginForm.find('[name="hasUninstallOptions"]').val() == 'true') {
+                  $.get('<?php echo $us_url_root; ?>usersc/plugins/' + $pluginForm.find('[name="plugin"]').val() + '/dialog.uninstall.html', function( response ) {
+                    $modelForm.find('.modal-body .plugin-options').html(response);
+                  });
+                }
+              });
+            </script>
           <?php } else { //pluginsC < 1
             echo "<br><strong>No plugins are currently installed.</strong>";
           } ?>
@@ -217,16 +364,13 @@ $token = Token::generate();
     $('.showTooltip').tooltip()
   })
 </script>
-<?php function pluginStatus($status)
-{
-  if ($status == 0) { ?>
-    <span class="text-primary">Installed but Disabled</span>
-  <?php
-    } elseif ($status == 1) { ?>
+<?php function pluginStatus($status){
+  if ($status == 1) { ?>
     <span class="text-success">Active</span>
+
   <?php
     } else { ?>
-    <span class="text-danger">Not Installed</span>
+    <span class="text-danger">Inactive</span>
 <?php
   }
 }
