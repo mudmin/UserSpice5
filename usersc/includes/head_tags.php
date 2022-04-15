96025c51a9736af9b28a36eb6b992001a23cfce9
Brandin Arsenault <brandin@brandinarsenault.com>
2022-04-15 14:41:47 -0300

    01479 - Convert head_tags Comments to PHP

--- usersc/includes/head_tags.php
+++ usersc/includes/head_tags.php
@@ -1,12 +1,15 @@
-<!-- Facebook sharing meta tags (delete if you don't want them) -->
-<!-- Be sure to update this in .11 -->
+<?php
+/*
+   Facebook sharing meta tags (delete if you don't want them)
+*/
+?>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <meta name="description" content="">
 <meta name="author" content="">
-<meta property="og:url"                content="" /> <!-- URL for website (link address) -->
-<meta property="og:type"               content="website" /> <!-- type of site -->
-<meta property="og:title"              content="Userspice Site" /> <!-- title of site (title of share) -->
-<meta property="og:description"        content="Powered by UserSpice" /> <!-- description of site (text which appears when sharing) -->
-<meta property="og:image"              content="" /> <!-- URL for preview image -->
+<meta property="og:url"             content="" /> <?php // URL for website (link address)?>
+<meta property="og:type"            content="website" /> <?php // type of site?>
+<meta property="og:title"           content="Userspice Site" /> <?php // title of site (title of share)?>
+<meta property="og:description"     content="Powered by UserSpice" /> <?php // description of site (text which appears when sharing)?>
+<meta property="og:image"           content="" /> <?php // URL for preview image?>
