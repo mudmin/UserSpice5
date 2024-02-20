240f55f6c9b5e0daf52d723519a896804f7c9e90
David Leruse <david.leruse@zebrawild.com>
2024-02-20 14:13:59 +0100

    new polyvalent Menu constructor  (menu identified by a numeric id or menu identified by its menu name)  + static function retrieveMenuIdByName +code  reformat

--- users/classes/Menu.php
+++ users/classes/Menu.php
@@ -1,7 +1,10 @@
 <?php
-class Menu {
+class Menu
+{
   protected $db;
-  public $id, $menu_name, $menu;
+  public $id;
+  public $menu_name;
+  public $menu;
   public $items = [];
   public $tree = [];
   public $userPerms = [0];
@@ -10,21 +13,26 @@ class Menu {
   public $show_active = 0;
   public $disabled = 0;
 
-  public function __construct($id) {
+  public function __construct($idormenuname)
+  {
     $this->db = DB::getInstance();
-    $q = $this->db->query("SELECT * FROM us_menus WHERE id = ?",[$id]);
+    if (is_int($idormenuname))
+      $q = $this->db->query("SELECT * FROM us_menus WHERE id = ?", [$idormenuname]);
+    else
+      $q = $this->db->query("SELECT * FROM us_menus WHERE menu_name = ?", [$idormenuname]);
+
     $c = $q->count();
-    if($c < 1){
+    if ($c < 1) {
       die("Your menu is missing. If you have just upgraded UserSpice,
       please navigate to users/updates in your browser to create your menus.
       Otherwise, please go into your database and restore a backup or select a different menu.
       If you do not have a backup, you can also create a UserSpice file and run the function migrateUSMainMenu() to
       attempt to create a new Main Menu");
-    }else{
+    } else {
       $menu = $q->first();
     }
 
-    if(!$menu) return false;
+    if (!$menu) return false;
     $this->menu = $menu;
     $this->id = $menu->id;
     $this->menu_name = $menu->menu_name;
@@ -34,40 +42,52 @@ class Menu {
     $this->_loadItems();
     $this->tree = $this->_loadTree(0);
     global $user;
-    if($user->isLoggedIn()) {
+    if ($user->isLoggedIn()) {
       //clear out the default 0 perm that non logged in users have
-      $perms = $this->db->query("SELECT * FROM user_permission_matches WHERE user_id = ?",[$user->data()->id])->results();
-      foreach($perms as $perm) {
+      $perms = $this->db->query("SELECT * FROM user_permission_matches WHERE user_id = ?", [$user->data()->id])->results();
+      foreach ($perms as $perm) {
         $this->userPerms[] = $perm->permission_id;
       }
-      if(pluginActive("usertags",true)){
-        $tags = $this->db->query("SELECT * FROM plg_tags_matches WHERE user_id = ?",[$user->data()->id])->results();
+      if (pluginActive("usertags", true)) {
+        $tags = $this->db->query("SELECT * FROM plg_tags_matches WHERE user_id = ?", [$user->data()->id])->results();
 
-        foreach($tags as $tag) {
+        foreach ($tags as $tag) {
           $this->userTags[] = $tag->tag_id;
         }
- 
       }
     }
   }
 
-  public function display($override = []) {
-    if(isset($override["layout"]) && ($override["layout"] == "horizontal" || $override["layout"] == "vertical" || $override["layout"] == "accordion") ){
-        $this->menu->type = $override["layout"] ;
+  public static function retrieveMenuIdByName($menu_name)
+  {
+    $db = DB::getInstance();
+    $q = $db->query("SELECT id FROM us_menus WHERE menu_name = ?", [$menu_name]);
+    $c = $q->count();
+    if ($c < 1) {
+      return -1;
+    } else {
+      $menu = $q->first();
+      return $menu->id;
     }
-    if(isset($override["show_active"])){
+  }
+  public function display($override = [])
+  {
+    if (isset($override["layout"]) && ($override["layout"] == "horizontal" || $override["layout"] == "vertical" || $override["layout"] == "accordion")) {
+      $this->menu->type = $override["layout"];
+    }
+    if (isset($override["show_active"])) {
       $this->menu->show_active = $override["show_active"];
     }
 
-    if(isset($override["branding_html"])){
+    if (isset($override["branding_html"])) {
       $this->menu->brand_html = $override["branding_html"];
     }
 
-    if(isset($override["show_branding"]) && $override["show_branding"] == false){
+    if (isset($override["show_branding"]) && $override["show_branding"] == false) {
       $this->show_branding = false;
     }
 
-    if(isset($override["theme"]) && $override["theme"] == false){
+    if (isset($override["theme"]) && $override["theme"] == false) {
       $this->menu->theme = $override["theme"];
     }
 
@@ -75,31 +95,34 @@ class Menu {
     echo $html;
   }
 
-  public function generate() {
+  public function generate()
+  {
     $html = $this->_generateHtml($this->tree, false);
     return "<nav>" . $html . "</nav>";
   }
 
-  public function recursivelyDeleteMenuItem($itemId) {
+  public function recursivelyDeleteMenuItem($itemId)
+  {
     $children = $this->_loadTree($itemId);
-    foreach($children as $child) {
+    foreach ($children as $child) {
       $this->recursivelyDeleteMenuItem($child->id);
     }
     $this->db->deleteById('us_menu_items', $itemId);
   }
 
-  private function hasPerms($item) {
+  private function hasPerms($item)
+  {
     global $user;
 
     $itemPerms = json_decode($item->permissions ?? "", true);
-    if($itemPerms == "") $itemPerms = [];
+    if ($itemPerms == "") $itemPerms = [];
     $itemTags = json_decode($item->tags ?? "", true);
-    if($itemTags == "") $itemTags = [];
+    if ($itemTags == "") $itemTags = [];
 
     // Once a user is logged in, the 0 permission on the item does not mean anything
     // and should be removed
     if ($user->isLoggedIn() && in_array("0", $itemPerms)) {
-        unset($itemPerms[array_search("0", $itemPerms)]);
+      unset($itemPerms[array_search("0", $itemPerms)]);
     }
 
     // Check if the user has any permissions in common with the item's permissions
@@ -110,48 +133,49 @@ class Menu {
 
     // Return true if either permission or tag is present
     return $hasPermission || $hasTag;
-}
+  }
 
-  private function _generateHtml($items, $isDropdown = false, $level = 0) {
-    global $abs_us_root,$us_url_root,$lang;
+  private function _generateHtml($items, $isDropdown = false, $level = 0)
+  {
+    global $abs_us_root, $us_url_root, $lang;
     $uniq = "_" . uniqid();
     // $uniq = "";
     $level++;
-    $ulClass = $isDropdown? "us_sub-menu": "us_menu";
-    $z = $this->menu->z_index?? 50;
+    $ulClass = $isDropdown ? "us_sub-menu" : "us_menu";
+    $z = $this->menu->z_index ?? 50;
     $ulStyle = " z-index: {$z};";
-    $menuId = $level == 1? "id='us_menu_{$this->menu->id}{$uniq}' data-menu_id='{$this->menu->id}'" : "";
-    if($level == 1) {
+    $menuId = $level == 1 ? "id='us_menu_{$this->menu->id}{$uniq}' data-menu_id='{$this->menu->id}'" : "";
+    if ($level == 1) {
       $ulClass .= " " . $this->menu->type;
     }
-    if($level == 1 && $this->menu->theme == 'dark') {
+    if ($level == 1 && $this->menu->theme == 'dark') {
       $ulClass .= " dark";
     }
 
-    if($level == 1 && !empty($this->menu->nav_class)) {
+    if ($level == 1 && !empty($this->menu->nav_class)) {
       $ulClass .= " {$this->menu->nav_class}";
     }
 
-    if($level > 2) {
+    if ($level > 2) {
       $ulClass .= " us_deep-sub-menu";
     }
     reset($items);
     $firstKey = key($items);
-    $ulId = $isDropdown? "menu_{$items[$firstKey]->menu}_dropdown_{$items[$firstKey]->parent}" : false;
-    $labelledBy = $isDropdown? "aria-labelledby='{$ulId}'" : '';
+    $ulId = $isDropdown ? "menu_{$items[$firstKey]->menu}_dropdown_{$items[$firstKey]->parent}" : false;
+    $labelledBy = $isDropdown ? "aria-labelledby='{$ulId}'" : '';
     $html = "<ul class='{$ulClass}' {$labelledBy} style='{$ulStyle}' {$menuId}>";
 
-    if($level == 1) {
-      $brandHtml = !empty($this->menu->brand_html)? html_entity_decode($this->menu->brand_html, ENT_QUOTES, 'UTF-8') : '';
-      $brandHtml = str_replace("{{root}}",$us_url_root,$brandHtml);
-      if($this->show_branding == false){
+    if ($level == 1) {
+      $brandHtml = !empty($this->menu->brand_html) ? html_entity_decode($this->menu->brand_html, ENT_QUOTES, 'UTF-8') : '';
+      $brandHtml = str_replace("{{root}}", $us_url_root, $brandHtml);
+      if ($this->show_branding == false) {
         $brandHtml = "";
       }
 
       $html .= "<div class='us_brand full_screen'>{$brandHtml}</a></div>";
 
-      if($this->menu->justify == "right"){
-          $html .= "<div class='flex-grow-1'></div>";
+      if ($this->menu->justify == "right") {
+        $html .= "<div class='flex-grow-1'></div>";
       }
 
 
@@ -159,9 +183,9 @@ class Menu {
 
 
       $html .= "<span class='additional-mobile-icons'>";
-            $mobile_menu_hooks = $abs_us_root . $us_url_root . "usersc/hooks/mobile_menu/";
+      $mobile_menu_hooks = $abs_us_root . $us_url_root . "usersc/hooks/mobile_menu/";
       $php_files = glob($mobile_menu_hooks . "*.php");
-      
+
       foreach ($php_files as $php_file) {
         ob_start();
         include $php_file;
@@ -171,48 +195,47 @@ class Menu {
       }
       $html .= "</span>";
       $html .= "<div class='us_menu_mobile_control' data-target='{$this->menu->id}{$uniq}'><i class='fa fa-bars'></i></div></div>";
- 
     }
-    foreach($items as $item) {
+    foreach ($items as $item) {
       // dump(parseMenuLabel($item->label));
       // dump($this->hasPerms($item));
       // dump("********************************************");
-      if(!$this->hasPerms($item) || $item->disabled == 1) continue;
-      if($item->type == 'separator') {
+      if (!$this->hasPerms($item) || $item->disabled == 1) continue;
+      if ($item->type == 'separator') {
         $html .= "<div class='dropdown-divider'></div>";
         continue;
       }
       $hasDropdown = sizeof($item->items) > 0;
-      $liClass = $hasDropdown? "dropdown" : "";
-      $liClass .= $item->li_class ? " $item->li_class": "";
+      $liClass = $hasDropdown ? "dropdown" : "";
+      $liClass .= $item->li_class ? " $item->li_class" : "";
 
       // check if the li should be active (i.e. its URL matches the current page)
       $currentPage = substr($_SERVER["REQUEST_URI"], strrpos($_SERVER["REQUEST_URI"], "/") + 1);
       $linkPage = substr($item->link, strrpos($item->link, "/") + 1);
 
-        if(($this->show_active == 1 || $this->show_active == true) && $currentPage == $linkPage){
-          $liClass .= " active active-style";
-        }elseif($currentPage == $linkPage){
-          $liClass .= " active";
-        }
+      if (($this->show_active == 1 || $this->show_active == true) && $currentPage == $linkPage) {
+        $liClass .= " active active-style";
+      } elseif ($currentPage == $linkPage) {
+        $liClass .= " active";
+      }
 
       // if($currentPage == $linkPage){
       //   $liClass .= " active";
       // }
       // build link
-      $linkClass = $hasDropdown? "sub-toggle" : "";
-      $linkClass .= $item->a_class? " $item->a_class": "";
-   
+      $linkClass = $hasDropdown ? "sub-toggle" : "";
+      $linkClass .= $item->a_class ? " $item->a_class" : "";
+
       $linkAttrs = " target='";
       $linkAttrs .= $item->link_target ? $item->link_target : "_self";
       $linkAttrs .= "' ";
-      
-      if($hasDropdown) {
+
+      if ($hasDropdown) {
         $toggle = "menu_{$item->menu}{$uniq}_dropdown_{$item->id}";
         $linkAttrs = "id='{$toggle}' role='button' aria-haspopup='true' aria-expanded='false' data-toggle='dropdown' data-target='#{$toggle}'";
       }
 
-      if($item->type == "snippet" && file_exists($abs_us_root . $us_url_root . $item->link)){
+      if ($item->type == "snippet" && file_exists($abs_us_root . $us_url_root . $item->link)) {
         //check file exists
 
         $html .= "";
@@ -223,31 +246,31 @@ class Menu {
         $html .=  $data;
         @ob_end_flush();
         $html .= "";
-      }else{
+      } else {
         $html .= "<li class='{$liClass}' data-menu='{$item->menu}'>";
-        if(strtolower(substr($item->link,0,5) != "http:") && strtolower(substr($item->link,0,6) != "https:")){
+        if (strtolower(substr($item->link, 0, 5) != "http:") && strtolower(substr($item->link, 0, 6) != "https:")) {
           $item->link = $us_url_root . $item->link;
         }
         $html .= "<a class='{$linkClass}' href='{$item->link}' {$linkAttrs}>";
-        if(!empty($item->icon_class)) {
+        if (!empty($item->icon_class)) {
           $html .= "<i class='{$item->icon_class}'></i>";
         }
 
         $parsedLabel = parseMenuLabel($item->label);
         if ($parsedLabel == ""  && isset($this->menu->screen_reader_mode) && $this->menu->screen_reader_mode == 1) {
-            $parsedLabel = lang("MENU_MENU");// for accessibility, don't allow blank labels - they are meaningless to screen readers
+          $parsedLabel = lang("MENU_MENU"); // for accessibility, don't allow blank labels - they are meaningless to screen readers
         }
         $html .= "<span class='labelText'>" . $parsedLabel . "</span>";
         // $html .= $item->label;
 
-        if($hasDropdown) {
+        if ($hasDropdown) {
           $html .= "<span class='caret fa fa-caret-down'></span>";
         }
         $html .= "</a>";
-        if($hasDropdown) {
+        if ($hasDropdown) {
           $html .= $this->_generateHtml($item->items, true, $level);
         }
-          $html .= "</li>";
+        $html .= "</li>";
       } //end non-snippet links
 
 
@@ -256,73 +279,80 @@ class Menu {
     return $html;
   }
 
-  public function miniMap($itemId) {
+  public function miniMap($itemId)
+  {
     $html = "<div class='minimap'>";
     $html .= $this->miniMapLinks($this->tree, $itemId, 0);
     $html .= "</div>";
     return $html;
   }
 
-  public function miniMapLinks($items, $itemId, $level) {
+  public function miniMapLinks($items, $itemId, $level)
+  {
     global $us_url_root;
     $html = "<ul>";
-    if($level == 0) {
-      $active = $itemId == 0? 'active': '';
+    if ($level == 0) {
+      $active = $itemId == 0 ? 'active' : '';
       $html .= "<li><a class='{$active}' href='{$us_url_root}users/admin.php?view=edit_menu&menu_id={$this->menu->id}&parent_id=0'>{$this->menu->menu_name}</a></li><ul>";
     }
-    foreach($items as $item) {
+    foreach ($items as $item) {
       $parsedLabel = parseMenuLabel($item->label);
 
       if ($parsedLabel == "" || $parsedLabel == null) {
-        if($item->type == "separator"){
+        if ($item->type == "separator") {
           $parsedLabel = "(separator)";
-        }else{
+        } else {
           $parsedLabel = "(no label)";
         }
-
       }
-      $active = $item->id == $itemId? 'active' : '';
+      $active = $item->id == $itemId ? 'active' : '';
       $html .= "<li>";
       $html .= "<a ";
-      if($item->disabled==1) { $html .= "style='color:lightgray'"; }
+      if ($item->disabled == 1) {
+        $html .= "style='color:lightgray'";
+      }
       $html .= " class='{$active}' href='{$us_url_root}users/admin.php?view=edit_menu&menu_id={$this->menu->id}&item_id={$item->id}&parent_id={$item->parent}'> $parsedLabel</a>";
-      if(!empty($item->items)) {
+      if (!empty($item->items)) {
         $level++;
         $html .= $this->miniMapLinks($item->items, $itemId, $level);
       }
       $html .= "</li>";
     }
-    if($level == 0) {
+    if ($level == 0) {
       $html .= "</ul>";
     }
     $html .= "</ul>";
     return $html;
   }
 
-  public function setInitialTypes() {
+  public function setInitialTypes()
+  {
     $this->_populateTypeColumn($this->tree);
   }
 
-  public function setInitialDisplayOrder() {
+  public function setInitialDisplayOrder()
+  {
     $this->_populateInitialDisplayOrder($this->tree);
   }
 
-  private function _populateInitialDisplayOrder($items) {
+  private function _populateInitialDisplayOrder($items)
+  {
     $i = 1;
-    foreach($items as $item) {
+    foreach ($items as $item) {
       $this->db->update('us_menu_items', $item->id, ['display_order' => $i]);
       $i++;
-      if(sizeof($item->items) > 0) {
+      if (sizeof($item->items) > 0) {
         $this->_populateInitialDisplayOrder($item->items);
       }
     }
   }
 
-  private function _populateTypeColumn($items) {
-    foreach($items as $item) {
-      if(empty($item->type)) {
+  private function _populateTypeColumn($items)
+  {
+    foreach ($items as $item) {
+      if (empty($item->type)) {
         $hasDropdown = sizeof($item->items) > 0;
-        if($hasDropdown) {
+        if ($hasDropdown) {
           $this->db->update('us_menu_items', $item->id, ['type' => 'dropdown']);
           $this->_populateTypeColumn($item->items);
         } else {
@@ -332,10 +362,11 @@ class Menu {
     }
   }
 
-  private function _loadTree($parent) {
+  private function _loadTree($parent)
+  {
     $children = [];
-    foreach($this->items as $item) {
-      if($item->parent == $parent) {
+    foreach ($this->items as $item) {
+      if ($item->parent == $parent) {
         $branch = clone $item;
         $branch->items = $this->_loadTree($item->id);
         $children[$item->id] = $branch;
@@ -344,8 +375,8 @@ class Menu {
     return $children;
   }
 
-  private function _loadItems(){
+  private function _loadItems()
+  {
     $this->items = $this->db->query("SELECT * FROM us_menu_items WHERE menu = ? ORDER BY display_order ASC", [$this->id])->results();
   }
-
 }
