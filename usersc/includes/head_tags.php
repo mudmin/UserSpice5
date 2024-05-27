f6ce504035bee85be70d9094dcdfc6c7f9b2db00
Dan Hoover <mudmin@gmail.com>
2024-05-27 05:56:00 -0400

    userspice head tags for full versions

--- usersc/includes/head_tags.php
+++ usersc/includes/head_tags.php
@@ -1,15 +1,21 @@
-<?php
-/*
-   Facebook sharing meta tags (delete if you don't want them)
-*/
-?>
+<?php //Social media sharing meta tags (delete if you don't want them) ?>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="">
 <meta name="author" content="">
-<meta property="og:url"             content="" /> <?php // URL for website (link address)?>
-<meta property="og:type"            content="website" /> <?php // type of site?>
-<meta property="og:title"           content="Userspice Site" /> <?php // title of site (title of share)?>
-<meta property="og:description"     content="Powered by UserSpice" /> <?php // description of site (text which appears when sharing)?>
-<meta property="og:image"           content="" /> <?php // URL for preview image?>
+
+<?php //URL for website (link address) ?>
+<meta property="og:url" content="">
+
+<?php //type of site ?>
+<meta property="og:type" content="website">
+
+<?php //title of site (title of share) ?>
+<meta property="og:title" content="Userspice Site">
+
+<?php //description of site (text which appears when sharing) ?>
+<meta property="og:description" content="Powered by UserSpice">
+
+<?php //URL for preview image ?>
+<meta property="og:image" content="">
