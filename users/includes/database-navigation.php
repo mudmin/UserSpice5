33c16769f224a95a656927c5332d322240a6b057
Dan Hoover <mudmin@gmail.com>
2026-03-19 16:37:10 -0400

    hotfix db nav

--- users/includes/database-navigation.php
+++ users/includes/database-navigation.php
@@ -1,5 +1,7 @@
 <?php
 require_once($abs_us_root.$us_url_root.'users/includes/template/database_navigation_prep.php');
-require_once($abs_us_root.$us_url_root.'users/includes/template/database_navigation_style.php');
+if(file_exists($abs_us_root.$us_url_root.'users/includes/template/database_navigation_style.php')){
+ require_once($abs_us_root.$us_url_root.'users/includes/template/database_navigation_style.php');
+}
 require_once($abs_us_root.$us_url_root.'users/includes/template/database_navigation_content.php');
 require_once($abs_us_root.$us_url_root.'users/includes/template/database_navigation_closing.php');
